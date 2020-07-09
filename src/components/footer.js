01406fd51efaf2a9d90f3cb029f96521111d4f63
André Martins <fmartins.andre@gmail.com>
2020-07-09 17:07:06 -0300

    adicionado imagem de fundo ao footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,13 +1,22 @@
 import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
 import { Grid, Typography, Link } from "@material-ui/core"
-import { makeStyles } from "@material-ui/styles"
+import BackgroundImage from "gatsby-background-image"
+import { styled, makeStyles } from "@material-ui/styles"
+
+const GradienBgImage = styled(BackgroundImage)({
+  opacity: "1!important",
+  backgroundSize: "cover",
+  background: "rgb(80,149,107)",
+  background:
+    "linear-gradient(45deg, rgba(80,149,107,0.95) 0%, rgba(46,96,162,0.95) 50%)",
+  backgroundSize: "cover",
+})
 
 const useStyles = makeStyles(theme => ({
   root: {
     padding: "40px 10px",
     color: "rgba(255,255,255,1)",
-    background:
-      "linear-gradient(45deg, rgba(46,96,162,0.9) 0%, rgba(80,149,107,0.9) 150%)",
   },
   smallText: {
     fontSize: "12px",
@@ -24,81 +33,105 @@ const useStyles = makeStyles(theme => ({
 
 const Footer = props => {
   const classes = useStyles(props)
+
+  const {
+    file: {
+      childImageSharp: { fluid: backgroundImage },
+    },
+  } = useStaticQuery(graphql`
+    query {
+      file(relativePath: { eq: "header_bg.jpg" }) {
+        childImageSharp {
+          fluid(quality: 80, maxWidth: 1366) {
+            ...GatsbyImageSharpFluid_withWebp
+          }
+        }
+      }
+    }
+  `)
+
   return (
-    <Grid
-      container
-      className={classes.root}
-      alignItems="center"
-      justify="space-around"
+    <GradienBgImage
+      Tag="footer"
+      id="footer"
+      fluid={backgroundImage}
+      backgroundColor={`linear-gradient(45deg, rgba(80,149,138,1) 0%, rgba(46,108,162,1) 100%)`}
     >
-      <Grid className={classes.gridItem} item xs={12} sm={6}></Grid>
-      <Grid className={classes.gridItem} item xs={12} sm={6}>
-        <Typography variant="subtitle2">Links Úteis</Typography>
-        <ul>
-          <li>
-            <Link
-              href="https://see.tjgo.jus.br/ajuda/publico"
-              target="_blank"
-              rel="noreferrer"
-              className={classes.link}
-            >
-              Sistema Extrajudicial Eletrônico
-            </Link>
-          </li>
-          <li>
-            <Link
-              href="https://see.tjgo.jus.br/buscas"
-              target="_blank"
-              rel="noreferrer"
-              className={classes.link}
-            >
-              Consultar Selos
-            </Link>
-          </li>
-          <li>
-            <Link
-              href="http://tjdocs.tjgo.jus.br/pastas/2101"
-              target="_blank"
-              rel="noreferrer"
-              className={classes.link}
-            >
-              Código de Normas e Procedimentos do Foro Extrajudicial
-            </Link>
-          </li>
-          <li>
-            <Link
-              href="https://www.google.com/maps/d/u/1/viewer?mid=1YBP3QGI5MjBJRj8ukcKHjesdvqMDUqhS"
-              target="_blank"
-              rel="noreferrer"
-              className={classes.link}
-            >
-              Mapa fronteiriço de Anápolis e da Primeira Circunscrição
-            </Link>
-          </li>
-        </ul>
-      </Grid>
       <Grid
-        className={classes.gridItem}
-        item
-        xs={12}
-        style={{ marginTop: "50px" }}
+        container
+        className={classes.root}
+        alignItems="center"
+        justify="space-around"
       >
-        <Typography className={classes.smallText} align="center">
-          Copyright © Cartório de Registro de Imóveis da 1º Circunscrição de
-          Anápolis/GO. All Rights Reserved.
-          <br />
-          Desenvolvido por{" "}
-          <Link
-            href="https://github.com/fmartins-andre"
-            target="_blank"
-            rel="noreferrer"
-            style={{ color: "#fff" }}
-          >
-            André Martins
-          </Link>
-        </Typography>
+        <Grid className={classes.gridItem} item xs={12} sm={6}></Grid>
+        <Grid className={classes.gridItem} item xs={12} sm={6}>
+          <Typography variant="subtitle2">Links Úteis</Typography>
+          <ul>
+            <li>
+              <Link
+                href="https://see.tjgo.jus.br/ajuda/publico"
+                target="_blank"
+                rel="noreferrer"
+                className={classes.link}
+              >
+                Sistema Extrajudicial Eletrônico
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="https://see.tjgo.jus.br/buscas"
+                target="_blank"
+                rel="noreferrer"
+                className={classes.link}
+              >
+                Consultar Selos
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="http://tjdocs.tjgo.jus.br/pastas/2101"
+                target="_blank"
+                rel="noreferrer"
+                className={classes.link}
+              >
+                Código de Normas e Procedimentos do Foro Extrajudicial
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="https://www.google.com/maps/d/u/1/viewer?mid=1YBP3QGI5MjBJRj8ukcKHjesdvqMDUqhS"
+                target="_blank"
+                rel="noreferrer"
+                className={classes.link}
+              >
+                Mapa fronteiriço de Anápolis e da Primeira Circunscrição
+              </Link>
+            </li>
+          </ul>
+        </Grid>
+        <Grid
+          className={classes.gridItem}
+          item
+          xs={12}
+          style={{ marginTop: "50px" }}
+        >
+          <Typography className={classes.smallText} align="center">
+            Copyright © Cartório de Registro de Imóveis da 1º Circunscrição de
+            Anápolis/GO. All Rights Reserved.
+            <br />
+            Desenvolvido por{" "}
+            <Link
+              href="https://github.com/fmartins-andre"
+              target="_blank"
+              rel="noreferrer"
+              style={{ color: "#fff" }}
+            >
+              André Martins
+            </Link>
+          </Typography>
+        </Grid>
       </Grid>
-    </Grid>
+    </GradienBgImage>
   )
 }
 
