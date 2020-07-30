ad54a3067f1364af3b98a79c51ba7434caee9b15
André Martins <fmartins.andre@gmail.com>
2020-07-30 16:27:14 -0300

    removidos arquivos da versão anterior

--- src/utils/smoothScroll.js
+++
@@ -1,9 +0,0 @@
-import scrollTo from "gatsby-plugin-smoothscroll"
-
-export default function smoothScroll(event) {
-  const anchor = event.currentTarget.href.split("#")
-  if (anchor.length > 1) {
-    event.preventDefault()
-    scrollTo(`#${anchor[1]}`)
-  }
-}
