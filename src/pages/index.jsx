73183456d22ce6c475c44186ce7b59e10c7dd25c
André Martins <fmartins.andre@gmail.com>
2020-08-24 11:28:03 -0300

    corrigida variável de ambiente do logrocket, para funcionar no frontend

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -8,7 +8,7 @@ import SectionLoadingFallback from "../components/sectionLoadingFallback"
 import LogRocket from "logrocket"
 import setupLogRocketReact from "logrocket-react"
 if (process.env.NODE_ENV === "production") {
-  LogRocket.init(process.env.LOGROCKET_APP_ID)
+  LogRocket.init(process.env.GATSBY_LOGROCKET_APP_ID)
   setupLogRocketReact(LogRocket)
 }
 
