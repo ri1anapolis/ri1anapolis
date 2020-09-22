206b49b02c6a625fac2366c51f31cc475b644f93
André Martins <fmartins.andre@gmail.com>
2020-09-22 17:00:45 -0300

    reorganizado o componente de documentos, para melhor legibilidade

--- src/pagesContent/documentosPanel2/index.jsx
+++ src/pagesContent/documentosPanel2/index.jsx
@@ -1,48 +1,13 @@
 import React from "react"
+
 import Typography from "@material-ui/core/Typography"
 import Grid from "@material-ui/core/Grid"
 import Container from "@material-ui/core/Container"
-import makeStyles from "@material-ui/styles/makeStyles"
+import Accordion from "../../components/simpleAccordion"
 
-import Accordion from "../components/simpleAccordion"
+import useStyles from "./styles"
+
 import documentos from "./documentosPanelContent"
-
-const useStyles = makeStyles(theme => ({
-  root: {
-    width: "100vw",
-    maxWidth: "500px",
-    minWidth: "300px",
-    height: "100%",
-    overflow: "hidden",
-    flexFlow: "column",
-    "& > div": {
-      overflowX: "hidden",
-      scrollbarWidth: "thin",
-      "&::-webkit-scrollbar": {
-        width: "6px",
-        height: "6px",
-        backgroundColor: "#F5F5F5",
-        overflowX: "auto",
-      },
-      "&::-webkit-scrollbar-thumb": {
-        borderRadius: "10px",
-        "-webkit-box-shadow": "inset 0 0 6px rgba(0,0,0,.3)",
-        backgroundColor: "#f0f0f0",
-        overflowX: "auto",
-      },
-    },
-  },
-  list: {
-    height: "100%",
-    overflowX: "hidden",
-    marginBottom: "10px",
-    overscrollBehavior: "none",
-    overscrollBehaviorBlock: "none",
-    "&> div:last-child": {
-      marginBottom: "50px !important",
-    },
-  },
-}))
 
 const DocumentosPanel = () => {
   const classes = useStyles()
