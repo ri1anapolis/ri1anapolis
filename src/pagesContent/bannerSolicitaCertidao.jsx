d7fb37364da5925e2385e0292cb3479ecbe3edf2
André Martins <fmartins.andre@gmail.com>
2020-10-09 17:10:21 -0300

    removida dependencia desnecessaria

--- src/pagesContent/bannerSolicitaCertidao.jsx
+++ src/pagesContent/bannerSolicitaCertidao.jsx
@@ -2,7 +2,6 @@ import React from "react"
 import Grid from "@material-ui/core/Grid"
 import Typography from "@material-ui/core/Typography"
 import Button from "@material-ui/core/Button"
-import Link from "@material-ui/core/Link"
 import makeStyles from "@material-ui/styles/makeStyles"
 import useTheme from "@material-ui/styles/useTheme"
 
