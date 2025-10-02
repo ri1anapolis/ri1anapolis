4e30d5b81da929a619c4587342097436e1709864
fmartins-andre <fmartins.andre@gmail.com>
2025-10-02 09:15:46 -0300

    feat: update contact information in maintenance alert for SearchReport

--- src/components/searchProtocol2/searchReport/index.jsx
+++ src/components/searchProtocol2/searchReport/index.jsx
@@ -30,13 +30,15 @@ const SearchReport = () => {
         <>
           <StyledAlertComponent severity="warning" title="EM MANUTENÇÃO">
             <Typography paragraph className={classes.hyphenate}>
-              Para consultar o status do seu processo{" "}
+              Para consultar o status do seu processo entre em contato pelo{" "}
               <Link
                 color="textPrimary"
                 href="https://api.whatsapp.com/send?phone=556239374650&text=Ol%C3%A1%2C%20gostaria%20de%20consultar%20o%20status%20do%20meu%20protocolol"
                 target="_blank"
               >
-                entre em contato pelo WhatsApp: (62) 3937-4650
+                <Button variant="contained" color="primary">
+                  WhatsApp: (62) 3937-4650
+                </Button>
               </Link>
             </Typography>
           </StyledAlertComponent>
